728e8d8faefcba15e963044d9cc945405e727d9a
Fernando-Gabreil-Tesori <ftesorie123@gmail.com>
2025-05-22 20:11:51 -0300

    explicação

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-let navigation = document.querySelector(".navigation");
-let close = document.querySelector(".close");
-navigation.onclick = function () {
-  navigation.classList.add("active");
+// Seleciona o elemento com a classe "navegacao"
+let navegacao = document.querySelector(".navegacao");
+
+// Seleciona o elemento com a classe "fechar"
+let fechar = document.querySelector(".fechar");
+
+// Quando o usuário clicar na navegação, adiciona a classe "ativo"
+navegacao.onclick = function () {
+  navegacao.classList.add("ativo");
 };
-close.onclick = function () {
-  navigation.classList.remove("active");
+
+// Quando o usuário clicar no botão de fechar, remove a classe "ativo"
+fechar.onclick = function () {
+  navegacao.classList.remove("ativo");
 };
